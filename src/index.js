85ee5f91f0798c1f902b76266a85fb406cf4be93
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-13 12:07:28 -0500

    resume at L53

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const App = () => {
-  return <div>Hi there!</div>
+// function-based component
+/*const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  )
+
+  return <div>Latitude: </div>
+}*/
+
+// class-based component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    )
+
+    return <div>Latitude: </div>
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
