d765c002ebd3db88faaf08fbd423d9e1aa174655
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-14 18:46:59 -0500

    class-based component with state initialization

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-// function-based component
-/*const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  )
-
-  return <div>Latitude: </div>
-}*/
-
-// class-based component
 class App extends React.Component {
-  /*
-  constructor(props) {
-    super(props)
-
-    this.state = { lat: null, errorMessage: "" }
-  }
-  */
-
   state = { lat: null, errorMessage: "" }
 
   componentDidMount() {
